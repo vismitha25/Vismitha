098e73c54d1e1ab248ad5caf86bd50c65190b407
“vismitha25” <vismithapriyanka152325.gmail.com>
2023-11-06 14:47:14 +0530

    Second version

--- test_calculator2.py
+++ test_calculator2.py
@@ -7,7 +7,7 @@ class CalculatorTest(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(9, self.calc.add(3, 7), "The addition is wrong")
+        self.assertEqual(10, self.calc.add(3, 7), "The addition is wrong")
 
     def test_subtract(self):
         self.assertEqual(12, self.calc.subtract(15, 3), "Subtraction is wrong")
